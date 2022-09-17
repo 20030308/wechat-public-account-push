8dd322169763856c067aa2f5af66a36978f8f98d
宗明德xx <113702278+20030308@users.noreply.github.com>
2022-09-17 16:45:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oKuZB50GBhhDUcKvVqkrEaX6QtkE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "dYN0rWKAw_dzVwTVKMoMmHgN_5kAb2kn5ZYP3TaSkNQ	",
+      useTemplateId: "dYN0rWKAw_dzVwTVKMoMmHgN_5kAb2kn5ZYP3TaSkNQ",
       // 所在省份
       province: "山东",
       // 所在城市
