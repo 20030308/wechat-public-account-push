c9355cf8fc0a8c24f7da7be476c40f2f5a413aed
宗明德xx <113702278+20030308@users.noreply.github.com>
2022-09-17 17:54:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oKuZB50GBhhDUcKvVqkrEaX6QtkE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "dYN0rWKAw_dzVwTVKMoMmHgN_5kAb2kn5ZYP3TaSkNQ",
+      useTemplateId: "YafF5rzFl3i5irCEeehLOoB5MhIi8A_Q--f9TVjrdZI",
       // 所在省份
       province: "山东",
       // 所在城市
@@ -97,7 +97,7 @@ export const config = {
     {
       name: "灿宝",
       id: "oKuZB5z_fPQT091_jIWbg67AMhTU",
-      useTemplateId: "IqVLaXMqrxnsqvksE8wulMDd8CGLcn4dg2Ums9Bpugo",
+      useTemplateId: "bDUkB9SltriJrIhPldsoIWPuYjy0aw1GTLlVGEaTmfg",
       province: "山东",
       city: "烟台",
       horoscopeDate: '10-03',
